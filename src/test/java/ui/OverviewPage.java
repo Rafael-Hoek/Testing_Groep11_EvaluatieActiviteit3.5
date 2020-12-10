f7619b915f9855b0d42a98328890a6543528f0b1
Stijn Hendrix <stijn_hendrix@yahoo.com>
2020-12-10 18:28:55 +0100

    overviewPage

---
+++ src/test/java/ui/OverviewPage.java
@@ -0,0 +1,24 @@
+package ui;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+
+import java.util.List;
+
+public class OverviewPage extends Page {
+    public OverviewPage(WebDriver driver) {
+        super(driver);
+    }
+
+
+    public boolean containsBroodje (String s) {
+        List<WebElement> trs = driver.findElements(By.cssSelector("td"));
+        for (WebElement tr: trs) {
+            if (tr.getText().equals(s)){
+                return true;
+            }
+        }
+        return false;
+    }
+}
--- src/test/java/ui/steps/ToonInfoAllergieënEnVegetarischSteps.java
+++ src/test/java/ui/steps/ToonInfoAllergieënEnVegetarischSteps.java
@@ -21,7 +21,7 @@ public class ToonInfoAllergieënEnVegetarischSteps {
         //System.setProperty("webdriver.chrome.driver", "/Users/.../web3pers/chromedriver");
         // windows: gebruik dubbele \\ om pad aan te geven
         // hint: zoek een werkende test op van web 2 ...
-        System.setProperty("webdriver.chrome.driver", "/Applications/chromedriver");
+        System.setProperty("webdriver.chrome.driver", "D:\\school\\chromedriver");
         driver = new ChromeDriver();
     }
 
