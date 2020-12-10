53ffa05c9fd376efaa618bb917876ab4f7e70c61
Herbert Gonzalez <herbert.gonzalezpimienta@student.ucll.be>
2020-12-10 18:12:16 +0100

    MealsPage added

---
+++ src/test/java/ui/MealsPage.java
@@ -0,0 +1,43 @@
+package ui;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+public class MealsPage extends Page{
+
+    public MealsPage(WebDriver driver) {
+        super(driver);
+    }
+
+    public void goToPage(){
+        this.driver.get(getPath()+"?command=Overview");
+    }
+
+    public boolean containsMealWithName(String name) {
+
+        List<WebElement> listItems= driver.findElements(By.cssSelector("table tr"));
+        List<String> names = Arrays.asList(name.split(" "));
+        boolean found = false;
+
+        for (WebElement listItem:listItems) {
+            String[] itemsList = listItem.getText().split(" ");
+            ArrayList<String> items = new ArrayList(Arrays.asList(itemsList));
+            if (items.containsAll(names)) {
+                found = true;
+            }
+        }
+        return found;
+    }
+
+    public boolean thereAreMeals() {
+        List<WebElement> listItems = this.driver.findElements(By.cssSelector("td"));
+        return !listItems.isEmpty();
+
+    }
+
+}
