2d67e6f143dfc92f14e02efa19e05cda3b39a506
Herbert Gonzalez <herbert.gonzalezpimienta@student.ucll.be>
2020-12-10 18:36:04 +0100

    meal test

--- src/test/java/ui/steps/ToonAlleMaaltijdenSteps.java
+++ src/test/java/ui/steps/ToonAlleMaaltijdenSteps.java
@@ -7,11 +7,19 @@ import io.cucumber.java.en.Then;
 import io.cucumber.java.en.When;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.PageFactory;
+import ui.MealsPage;
 import ui.Page;
+import ui.PatientsPage;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
 
 public class ToonAlleMaaltijdenSteps {
     private WebDriver driver;
     private String path = "http://localhost:8080/Controller";
+
+    private Page currentPage;
 
     @Before
     public void setUp() {
@@ -31,12 +39,14 @@ public class ToonAlleMaaltijdenSteps {
 
     @When("“Rafael” op het menu kijkt")
     public void Rafael_kijkt_op_het_menu() {
-
+        currentPage = PageFactory.initElements(driver, MealsPage.class);
     }
 
     @Then("worden alle maaltijden getoond die op het menu staan")
     public void alle_die_op_het_menu_staan_maaltijden_worden_getoond() {
-
+        assertEquals("Product Overview", driver.getTitle());
+        assertTrue(((MealsPage)currentPage).containsMealWithName("Lasagna"));
+        assertTrue(((MealsPage)currentPage).containsMealWithName("Tomatensoep"));
     }
 
     @Given("er geen maaltijden op het menu staan")
